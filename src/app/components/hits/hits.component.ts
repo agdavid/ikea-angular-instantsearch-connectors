6de64aff71f2a173b51c61325224c4720b39f45d
Ajay David <antonio.g.david@gmail.com>
2018-05-20 21:56:09 -0400

    access instantsearch service using connectors

--- src/app/components/hits/hits.component.ts
+++ src/app/components/hits/hits.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {connectHits } from 'instantsearch.js/es/connectors';
+
 import { InstantsearchService } from '../../services/instantsearch.service';
 
 @Component({
@@ -11,10 +13,30 @@ import { InstantsearchService } from '../../services/instantsearch.service';
   styles: []
 })
 export class HitsComponent implements OnInit {
+  // Define how your component state will look like,
+  // and intialize it with an empty hits array
+  state: { hits: {}[] } = { hits: [] };
 
-  constructor(private instantsearchService: InstantsearchService) { }
+  constructor(private instantsearchService: InstantsearchService) {}
 
   ngOnInit() {
+    // Create a widget which will call `this.updateState` whenever
+    // something changes on the search state itself
+    const widget = connectHits(this.updateState);
+
+    // Register the Hits widget into the instantSearchService search instance.
+    this.instantsearchService.search.addWidget(widget());
   }
 
+  updateState = (state, isFirstRendering) => {
+    // asynchronous update of the state
+    // avoid `ExpressionChangedAfterItHasBeenCheckedError`
+    if (isFirstRendering) {
+      return Promise.resolve().then(() => {
+        this.state = state;
+      });
+    }
+
+    this.state = state;
+  }
 }
