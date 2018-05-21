e6c31741b5b427159e7f4603042ed33377808f48
Ajay David <antonio.g.david@gmail.com>
2018-05-20 22:00:50 -0400

    start search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import { InstantsearchService } from './services/instantsearch.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Ikea Angular Instantsearch';
+
+  constructor(private instantsearchService: InstantsearchService) {}
+
+  ngAfterViewInit() {
+    this.instantsearchService.search.start();
+  }
+
 }
