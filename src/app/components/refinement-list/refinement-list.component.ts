ba973d28d8b7354e5c576fd7f5b6427f02058614
Ajay David <antonio.g.david@gmail.com>
2018-05-21 08:52:49 -0400

    inject search service into RefinmentListComponent and connect to search state

--- src/app/components/refinement-list/refinement-list.component.ts
+++ src/app/components/refinement-list/refinement-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { connectRefinementList } from 'instantsearch.js/es/connectors';
+
+import { InstantsearchService } from '../../services/instantsearch.service';
 
 @Component({
   selector: 'app-refinement-list',
@@ -11,9 +14,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RefinementListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private instantsearchService: InstantsearchService) { }
 
   ngOnInit() {
+    // Create a widget which will call 'this.updateState' whenever
+    // something changes on the search state itself
+    const widget = connectRefinementList(this.updateState);
+
+    // Register the RefinementList widget into the instantsearchService search instance
+    this.instantsearchService.search.addWidget(widget());
+  }
+
+  updateState = () => {
+
   }
 
 }
